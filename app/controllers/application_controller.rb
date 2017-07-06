f453b7da92bbb42bbb750a76e54baa3fd31c8d0c
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 15:08:10 +0000

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,20 @@ class ApplicationController < ActionController::Base
 
   
   before_filter :set_title
+  before_filter :set_copyright
   
   def set_title
     @page_title = "Devcamp Portfolio | My Portfolio Website"
   end
+  def set_copyright
+    @copyright = DynamicCopyright::Renderer.copyright 'Chance Carr', 'All rights reserved'
+  end
+end
 
+module DynamicCopyright
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+    end
 end
